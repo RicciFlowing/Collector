ea10a2e84241cbebdc29417ec0ebc2cd41022e53
RicciFlowing <benjamin@mathe-sellin.de>
2015-08-24 21:19:30 +0200

    Added the missing name collumn to the categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([{name: "geometry"},{name: "arithmetic"}, {name: "analysis"}, {name: "stochastic" }, {name:"alebgra"}]);
