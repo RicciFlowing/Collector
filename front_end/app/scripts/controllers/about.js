387eb2731e0e4cc012633f777166c98ccfa5b12b
RicciFlowing <benjamin@mathe-sellin.de>
2015-08-29 16:23:11 +0200

    Begin seperating the frontend code into an Angular app.
    Therfore:
    * Created a direcotry front_end to contain the code
    * Uses the yeoman angular generator to start the front end project
    * Removed some content dummy content from the controllers and views.

---
+++ front_end/app/scripts/controllers/about.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collectorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the collectorApp
+ */
+angular.module('collectorApp')
+  .controller('AboutCtrl', function () {
+  });
---
+++ front_end/app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name collectorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the collectorApp
+ */
+angular.module('collectorApp')
+  .controller('MainCtrl', function () {
+  });
