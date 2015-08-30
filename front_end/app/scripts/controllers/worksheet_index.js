b9489f3a24e7443f1f74ca507dbcce226458997f
RicciFlowing <benjamin@mathe-sellin.de>
2015-08-30 17:11:59 +0200

    Added a simple search field on the worksheets/indexpage

--- front_end/app/scripts/controllers/worksheet_index.js
+++ front_end/app/scripts/controllers/worksheet_index.js
@@ -15,6 +15,13 @@ angular.module('collectorApp')
                           klass: 7,
                           description: "A nice description",
                           category: {name:"Arihtmetic", subject:"Math" }
-                        }
+                        },
+                        {
+                          file: {url: "example.com"},
+                          klass: 8,
+                          description: "A nice description",
+                          category: {name:"Arihtmetic", subject:"Math" }
+                        },
+
     ];
   });
